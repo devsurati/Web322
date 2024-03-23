0decefa76c701ebd54eb8c9a22c76afc436638a6
Dev Surati <149343116+devsurati@users.noreply.github.com>
2024-03-22 23:01:33 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,82 +1,51 @@
-/********************************************************************************
-*  WEB322 - Assignment 03
-* 
-*  I declare that this assignment is my own work in accordance with Seneca's
-*  Academic Integrity Policy:
-* 
-*  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
-* 
-*  Name: Dev A. Surati Student ID: 117958223 Date: 24-02-16
-*  Published URL: https://dark-jade-gopher-veil.cyclic.app
-********************************************************************************/
+const legoData = require("./modules/legoSets");
+
+
+const path = require("path");
 
 const express = require('express');
 const app = express();
-const path = require("path");
-const legoData = require("./modules/legoSets");
-const HTTP_PORT = process.env.PORT || 8080
-app.use(express.static("public"));
 
-legoData.initialize()
-  .then(() => {
-    app.listen(HTTP_PORT, () => {
-      console.log(`Server is initialized on port http://localhost:${HTTP_PORT}`);
-    });
-  })
-  .catch(error => {
-    console.error('Failed to initialize legoData:', error);
-  });
+const HTTP_PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/home.html"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => res.render('home'));
+
+app.get('/about', (req, res) => res.render('about'));
+
+app.get("/lego/sets", async (req,res)=>{
+  try{
+    if(req.query.theme){
+      let sets = await legoData.getSetsByTheme(req.query.theme);
+      res.render("sets", { sets: sets });
+  
+    }else{
+      let sets = await legoData.getAllSets();
+      res.render("sets", { sets: sets });
+    }
+  }catch(err){
+    res.status(404).render("404", { message: "Unable to find requested set." });
+  }
+
 });
 
-app.get('/about', (req, res)=>{ 
-  res.sendFile(path.join(__dirname, "/views/about.html"));
-});
-
-app.get('/lego/sets', (req, res) => {
-  const theme = req.query.theme;
-
-  if (theme) {
-    legoData.getSetsByTheme(theme)
-      .then(sets => {
-        if (sets.length > 0) {
-          res.json(sets);
-        } else {
-          res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
-        }
-      })
-      .catch(error => {
-        res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
-      });
-  } else {
-    legoData.getAllSets()
-      .then(sets => {
-        res.json(sets);
-      })
-      .catch(error => {
-        res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
-      });
+app.get("/lego/sets/:num", async (req,res)=>{
+  try{
+    let set = await legoData.getSetByNum(req.params.num);
+    res.render("set", {set: set});
+  }catch(err){
+    res.status(404).render("404", { message: "Unable to find requested sets." });
   }
 });
 
-app.get('/lego/sets/:set_num', (req, res) => {
-  const setNum = req.params.set_num;
-
-  legoData.getSetByNum(setNum)
-    .then(sets => {
-      if (sets) {
-        res.json(sets);
-      } else {
-        res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
-      }
-    })
-    .catch(error => {
-      res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
-    });
+app.use((req, res, next) => {
+  res.status(404).render("404", { message: "I'm sorry, we're unable to find what you're looking for." });
 });
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+
+legoData.initialize().then(()=>{
+  app.listen(HTTP_PORT, () => { console.log(`server running on: http://localhost:${HTTP_PORT}`) });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [`./views/*.html`],
+  content: [`./views/**/*.ejs`],
   theme: {
     extend: {},
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: ['cupcake'],
+    themes: ['fantasy'],
   },
 }
-
